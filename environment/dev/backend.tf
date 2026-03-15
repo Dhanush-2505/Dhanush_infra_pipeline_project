e650c35d3674a8298b7194a738231ad8992efb04
dhanush <dhanushnagarajan1976@gmail.com>
2026-03-15 12:19:50 +0530

    make some chages dev files and jenkins file

---
+++ environment/dev/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.67.0"                # Specify the version of the AWS provider to use
+    }
+  }
+  backend "s3" {
+    bucket = "infra-pipeline-bu"
+    key    = "environments/dev/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
